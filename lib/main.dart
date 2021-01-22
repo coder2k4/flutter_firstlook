ee42603f2963f5501c1a11c66af835f88e1b760a
Standalone G.A.S <coder2k@mail.ru>
2021-01-22 20:42:08 +0300

    Scaffold style

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
+
+// void main() {
+//   runApp(MyApp());
+// }
 
 class MyApp extends StatelessWidget {
-  Widget build (BuildContext context) {
-    return MaterialApp(home: Text('Hello'),);
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Статус бар'),
+        ),
+        body: Text('Тело'),
+      ),
+    );
   }
 }
